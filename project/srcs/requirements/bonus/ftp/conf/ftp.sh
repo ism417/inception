2904a9d6a3d47ee23e0a521e7b135acd58b18bac
ism417 <ielab036@gmail.com>
2025-10-04 09:49:12 +0100

    fin

--- project/srcs/requirements/bonus/ftp/conf/ftp.sh
+++ project/srcs/requirements/bonus/ftp/conf/ftp.sh
@@ -12,7 +12,13 @@ fi
 sed -i "s|#write_enable=YES|write_enable=YES|g" /etc/vsftpd.conf
 sed -i "s|#chroot_local_user=YES|chroot_local_user=YES|g" /etc/vsftpd.conf
 echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
+echo "local_enable=YES" >> /etc/vsftpd.conf
+
+# Set proper ownership and permissions
+chown -R ${FTP_USER}:${FTP_USER} /var/www/wordpress
+chmod -R 755 /var/www/wordpress
+
 mkdir -p /var/run/vsftpd/empty
 usermod -d /var/www/wordpress ${FTP_USER}
-chmod 777 /var/www/wordpress
+
 exec "$@"
--- project/srcs/requirements/mariadb/conf/mdb-conf.sh
+++ project/srcs/requirements/mariadb/conf/mdb-conf.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Initialize MariaDB if not already initialized
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
 #--------------mariadb start--------------#
 service mariadb start # start mariadb
 sleep 5 # wait for mariadb to start
